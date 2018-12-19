ecb3a087df1ba61c5ad9e03b58bffdc935099fd7
Laon21 <31162065+Laon21@users.noreply.github.com>
2018-12-19 13:42:29 +0100

    template scene done

--- Smidig gui/Assets/Scripts/Scene1text.cs
+++ Smidig gui/Assets/Scripts/Scene1text.cs
@@ -6,9 +6,15 @@ public class Scene1text : MonoBehaviour
 {
  public GameObject snakkeboble;
  public GameObject valg;
+ public GameObject snakkebobleTekst;
 
  public void onClick(){
      snakkeboble.SetActive(false);
      valg.SetActive(true);
+ }
+
+ public void returnNavBar(){
+     snakkeboble.SetActive(true);
+     snakkebobleTekst.SetActive(false);
  }
 }
