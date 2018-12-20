82e237120e7250d1cb08631469ed85c665a30b57
Laon21 <31162065+Laon21@users.noreply.github.com>
2018-12-20 21:26:21 +0100

    first and second choice shows up, doesnt check what path you're on. Some problem with the text aswell on the second choice where it doesnt remove the player reply and replaces some of the npc dialog

--- Smidig gui/Assets/Scripts/DialogManager.cs
+++ Smidig gui/Assets/Scripts/DialogManager.cs
@@ -6,16 +6,16 @@ using UnityEngine.UI;
 public class DialogManager : MonoBehaviour
 {
     public GameObject skuespillerTekst;
-    public GameObject SpillerTekst;
     public GameObject valgUi;
     public Text nameText;
     public Text dialogText;
-    public Text spillerText;
     public Text Choice1;
     public Text Choice2;
     private Queue<string> sentence;
     private Queue<string> choice;
-    public Animator animator;
+    public Animator animator;        
+    int SentenceCount;
+
 
     void Start(){
     sentence = new Queue<string>();
@@ -29,6 +29,7 @@ public class DialogManager : MonoBehaviour
         nameText.text = dialog.navn;
         sentence.Clear();
         choice.Clear();
+        SentenceCount = 0;
 
         foreach(string line in dialog.sentences){
             sentence.Enqueue(line);
@@ -42,20 +43,22 @@ public class DialogManager : MonoBehaviour
     }
 
     public void DisplayNextSentence() {
-        if(sentence.Count == 0){
+        if(sentence.Count == 0 || SentenceCount == 1 || SentenceCount == 3){
             if(choice.Count != 0){
             skuespillerTekst.SetActive(false);
             Choice1.text = choice.Dequeue();
             Choice2.text = choice.Dequeue();
             valgUi.SetActive(true);
-            EndDialog();
-            return;
             }
         }
+        
 
         string dialogLine = sentence.Dequeue();
         StopAllCoroutines();
         StartCoroutine(TypeSentence(dialogLine));
+        Debug.Log(SentenceCount);
+        SentenceCount++;
+        
     }
 
     IEnumerator TypeSentence (string sentence){
@@ -68,8 +71,8 @@ public class DialogManager : MonoBehaviour
 
     public void DisplayChoiceText(GameObject choice){
         valgUi.SetActive(false);
-        spillerText.text = choice.GetComponent<Text>().text;
-        SpillerTekst.SetActive(true);
+        dialogText.text = choice.GetComponent<Text>().text;
+        skuespillerTekst.SetActive(true);
     }
 
     void EndDialog(){
