dba4e60fe8493ce579fbc3243b3d7bdf6c1b2bba
Henrik <hsn95@live.com>
2018-12-21 04:15:46 +0100

    Mye updates
    Hva som er gjort er skrevet på Discord

--- Smidig gui/Assets/Scripts/DialogManager.cs
+++ Smidig gui/Assets/Scripts/DialogManager.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using System;
+using System.Text;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,15 +13,17 @@ public class DialogManager : MonoBehaviour
     public Text dialogText;
     public Text Choice1;
     public Text Choice2;
-    private Queue<string> sentence;
-    private Queue<string> choice;
-    public Animator animator;        
-    int SentenceCount;
+    private List<string> sentence;
+    private List<string> choice;
+    public Animator animator;
+	string nextDialogueElement;
+	string choice1Info;
+	string choice2Info;
 
 
-    void Start(){
-    sentence = new Queue<string>();
-    choice = new Queue<string>();
+	void Start(){
+    sentence = new List<string>();
+    choice = new List<string>();
     }
 
     public void StartDialog(Dialog dialog, Choices choices){
@@ -29,35 +33,41 @@ public class DialogManager : MonoBehaviour
         nameText.text = dialog.navn;
         sentence.Clear();
         choice.Clear();
-        SentenceCount = 0;
+		nextDialogueElement = "s0";
 
-        foreach(string line in dialog.sentences){
-            sentence.Enqueue(line);
+
+		foreach (string line in dialog.sentences){
+            sentence.Add(line);
         }
 
         foreach(string line in choices.choices){
-            choice.Enqueue(line);
-        }
+			choice.Add(line);
+		}
 
         DisplayNextSentence();
     }
 
     public void DisplayNextSentence() {
-        if(sentence.Count != 0 || choice.Count != 0){
-            if(SentenceCount == 0 || SentenceCount == 2 || SentenceCount == 4 || SentenceCount == 6 
-                || SentenceCount == 7 || SentenceCount == 9 || SentenceCount == 11 || SentenceCount == 13 || SentenceCount == 15){
-              string dialogLine = sentence.Dequeue();
-              StopAllCoroutines();
-               StartCoroutine(TypeSentence(dialogLine));
-               Debug.Log(SentenceCount);
-              SentenceCount++;
-              return;
-            }
-        skuespillerTekst.SetActive(false);
-        Choice1.text = choice.Dequeue();
-        Choice2.text = choice.Dequeue();
-        valgUi.SetActive(true);
+		bool nextIsSentence = nextDialogueElement[0] == 's';
+		int elementIndex = Int32.Parse(nextDialogueElement.Substring(1));
+
+		if (nextIsSentence)
+		{
+			string[] info = sentence[elementIndex].Split(':');
+			string dialogLine = info[0];
+			nextDialogueElement = info[1];
+			StopAllCoroutines();
+            StartCoroutine(TypeSentence(dialogLine));
+            Debug.Log(elementIndex);
+            return;
         }
+        skuespillerTekst.SetActive(false);
+		string[] alternatives = choice[elementIndex].Split('=');
+		choice1Info = alternatives[0];
+		choice2Info = alternatives[1];
+		Choice1.text = choice1Info.Split(':')[0];
+        Choice2.text = choice2Info.Split(':')[0];
+        valgUi.SetActive(true);
     }
 
     IEnumerator TypeSentence (string sentence){
@@ -70,9 +80,19 @@ public class DialogManager : MonoBehaviour
 
     public void DisplayChoiceText(GameObject choice){
         valgUi.SetActive(false);
-        dialogText.text = choice.GetComponent<Text>().text;
+		string text = choice.GetComponent<Text>().text;
+		string[] info1 = choice1Info.Split(':');
+		if (text.Equals(info1[0]))
+		{
+			nextDialogueElement = info1[1];
+			Debug.Log(nextDialogueElement);
+		}
+		else
+		{
+			nextDialogueElement = choice2Info.Split(':')[1];
+		}
+		dialogText.text = text;
         skuespillerTekst.SetActive(true);
-        SentenceCount++;
 
     }
 
